209dbfd2bf6687ee208fb75a158de12b37719596
wangxincheng <xincheng.wang@hand-china.com>
2021-02-27 00:30:04 +0800

    123

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import {Provider} from 'react-redux'
 import store from './store/index.js'
 
-
+//heml
 ReactDOM.render(<Provider store={store}> <App /> </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
