cb64c8b2ad33582fc68d1d26f1f88394a9b3f2e4
wangxincheng <xincheng.wang@hand-china.com>
2021-02-27 13:49:47 +0800

    Updates

---
+++ precache-manifest.f70b525f070355d41ba61298c92f18f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9239487a8d75d962c09f23b30768b088",
+    "url": "/react-demo/index.html"
+  },
+  {
+    "revision": "2750fd81c2e1a3cbc85c",
+    "url": "/react-demo/static/css/2.7042f68b.chunk.css"
+  },
+  {
+    "revision": "23a2e669484011db60fb",
+    "url": "/react-demo/static/css/main.80713b16.chunk.css"
+  },
+  {
+    "revision": "2750fd81c2e1a3cbc85c",
+    "url": "/react-demo/static/js/2.d3f11353.chunk.js"
+  },
+  {
+    "revision": "23a2e669484011db60fb",
+    "url": "/react-demo/static/js/main.0c2dd40d.chunk.js"
+  },
+  {
+    "revision": "55dad1cca55e95e8bb36",
+    "url": "/react-demo/static/js/runtime-main.03032275.js"
+  }
+]);
